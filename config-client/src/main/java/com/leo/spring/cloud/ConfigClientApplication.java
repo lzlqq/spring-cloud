4ebbb1e0e25da0bf6235e3302855571a8a54ad63
lzlqq <390650599@qq.com>
2019-03-14 01:04:36 +0800

    SpringApplication启动参数自定义

--- config-client/src/main/java/com/leo/spring/cloud/ConfigClientApplication.java
+++ config-client/src/main/java/com/leo/spring/cloud/ConfigClientApplication.java
@@ -1,4 +1,14 @@
 package com.leo.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication springApplication=new SpringApplication(ConfigClientApplication.class);
+        springApplication.setWebEnvironment(true);
+        springApplication.run(args);
+       // SpringApplication.run(ConfigClientApplication.class,args);
+    }
 }
