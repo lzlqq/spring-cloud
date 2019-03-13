b1ce67a946da53050ca568d36d2597e373d587fb
lzlqq <390650599@qq.com>
2019-03-14 00:21:35 +0800

    Spring 监听器带上下文版

--- config-client/src/main/java/com/leo/spring/cloud/demo/SpringEventListenerDemo.java
+++ config-client/src/main/java/com/leo/spring/cloud/demo/SpringEventListenerDemo.java
@@ -1,5 +1,6 @@
 package com.leo.spring.cloud.demo;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringEventListenerDemo {
     public static void main(String[] args) {
         //Annotation 驱动的Spring上下文
-        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         //注册监听器
         context.addApplicationListener(new ApplicationListener<MyApplicationEvent>() {
             /**
@@ -19,19 +20,26 @@ public class SpringEventListenerDemo {
              */
             @Override
             public void onApplicationEvent(MyApplicationEvent applicationEvent) {
-                System.out.println(applicationEvent.getSource());
+                System.out.println(applicationEvent.getSource()+"--->"+applicationEvent.getApplicationContext());
             }
         });
         //刷新上下文
         context.refresh();
         //发布事件
-        context.publishEvent(new MyApplicationEvent("hello"));
-        context.publishEvent(new MyApplicationEvent(666));
+        context.publishEvent(new MyApplicationEvent(context,"hello"));
+        context.publishEvent(new MyApplicationEvent(context,666));
     }
-    private static class MyApplicationEvent extends ApplicationEvent{
 
-        public MyApplicationEvent(Object source) {
+    private static class MyApplicationEvent extends ApplicationEvent {
+        private final ApplicationContext applicationContext;
+
+        public MyApplicationEvent(ApplicationContext applicationContext, Object source) {
             super(source);
+            this.applicationContext = applicationContext;
+        }
+
+        public ApplicationContext getApplicationContext() {
+            return applicationContext;
         }
     }
 }
