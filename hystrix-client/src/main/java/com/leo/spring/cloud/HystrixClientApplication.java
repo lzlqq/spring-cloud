3b4563a637a59d0a452333b44539a269d9253515
lzlqq <390650599@qq.com>
2019-03-24 17:11:39 +0800

    Hystrix Dashboard

--- hystrix-client/src/main/java/com/leo/spring/cloud/HystrixClientApplication.java
+++ hystrix-client/src/main/java/com/leo/spring/cloud/HystrixClientApplication.java
@@ -2,10 +2,12 @@ package com.leo.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableHystrix
+//@EnableHystrix
+@EnableCircuitBreaker
 public class HystrixClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixClientApplication.class, args);
