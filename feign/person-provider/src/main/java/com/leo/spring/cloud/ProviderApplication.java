0a8402cf47df812c7faf62c41be46b97df48b947
lzlqq <390650599@qq.com>
2019-03-28 23:20:30 +0800

    Zuul 整合Hystrix

--- feign/person-provider/src/main/java/com/leo/spring/cloud/ProviderApplication.java
+++ feign/person-provider/src/main/java/com/leo/spring/cloud/ProviderApplication.java
@@ -3,12 +3,14 @@ package com.leo.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * 服务提供方
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class,args);
--- feign/person-provider/src/main/java/com/leo/spring/cloud/controller/PersonProviderController.java
+++ feign/person-provider/src/main/java/com/leo/spring/cloud/controller/PersonProviderController.java
@@ -2,13 +2,17 @@ package com.leo.spring.cloud.controller;
 
 import com.leo.spring.cloud.domain.Person;
 import com.leo.spring.cloud.service.PersonService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -17,15 +21,33 @@ import java.util.concurrent.ConcurrentHashMap;
 @RestController
 public class PersonProviderController {
 
-    private Map<Long, Person> persons=new ConcurrentHashMap<>();
+    private Map<Long, Person> persons = new ConcurrentHashMap<>();
+    private final Random random = new Random();
 
     @PostMapping(value = "/person/save")
-    public boolean save(@RequestBody Person person){
-        return persons.put(person.getId(),person)==null;
+    public boolean save(@RequestBody Person person) {
+        return persons.put(person.getId(), person) == null;
     }
 
     @GetMapping(value = "/person/find/all")
-    public Collection<Person> findAll(){
+    @HystrixCommand(fallbackMethod = "fallbackForFindAllPersons", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+                    value = "100")
+    })
+    public Collection<Person> findAll() throws InterruptedException {
+        //超时触发容错
+        int value =random.nextInt(200);
+        Thread.sleep(value);
+        System.out.println("findAll() costs "+value+" ms.");
         return persons.values();
+    }
+
+    /**
+     * {@link #findAll()} Fallback 方法
+     * @return 返回空集合
+     */
+    public  Collection<Person> fallbackForFindAllPersons(){
+        System.out.println("fallbackForFindAllPersons() is invoked");
+        return Collections.emptyList();
     }
 }
