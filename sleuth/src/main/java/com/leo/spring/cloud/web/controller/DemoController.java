237a700a1f1ed88a7b075d1d4d765189686ddfab
lzlqq <390650599@qq.com>
2019-04-07 16:24:51 +0800

    Sleuth 简单应用

---
+++ sleuth/src/main/java/com/leo/spring/cloud/web/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.leo.spring.cloud.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    protected final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("")
+    public String index() {
+        String returnValue = "Hello World";
+        logger.info("{} index() : {}", getClass().getSimpleName(), returnValue);
+        return returnValue;
+    }
+}
